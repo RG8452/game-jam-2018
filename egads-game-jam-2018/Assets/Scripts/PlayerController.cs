eeb6d793020b703677b4c4951dc064f6a47a2e31
toki1 <43299164+toki1@users.noreply.github.com>
2018-09-15 12:50:41 -0700

    playercontroll

--- egads-game-jam-2018/Assets/Scripts/PlayerController.cs
+++ egads-game-jam-2018/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    int playerDirection;
 	void Start()
 	{
-		
-	}
+        playerDirection = 0;
+    }
 
 	void Update()
 	{
-	}
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 270);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+            
+        }
+    }
 }
