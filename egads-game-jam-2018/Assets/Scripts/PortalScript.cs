141b4b08daba31cffb842d86da52da4068718c7f
Tony Nguyen <blue28275@yahoo.com>
2018-09-15 14:24:04 -0500

    Prototype 1

---
+++ egads-game-jam-2018/Assets/Scripts/PortalScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+	public GameObject[] portals;
+	public GameObject projectilePrefab;
+	GameObject projectile;
+
+	void Start()
+	{
+		portals = GameObject.FindGameObjectsWithTag("Portal");
+		int point = Random.Range(0, portals.Length - 1);
+		GameObject spawnPoint = portals[point];
+		projectile = Instantiate(projectilePrefab);
+
+	}
+
+	void Update()
+	{
+		
+	}
+}
---
+++ egads-game-jam-2018/Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+	public float moveSpeed;
+
+	void Awake()
+	{
+	}
+
+	void Start()
+	{
+		moveSpeed = 1f;
+	}
+
+	void Update()
+	{
+		float moveHorizontal = moveSpeed * Time.deltaTime;
+
+		transform.Translate(new Vector3(moveHorizontal, 0, 0));
+	}
+}
