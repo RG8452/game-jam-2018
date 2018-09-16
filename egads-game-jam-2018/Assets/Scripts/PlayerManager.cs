27529607b96679e501364dbab14823c0a14a7291
AndrewWang333 <andrew.wang333@gmail.com>
2018-09-15 20:21:30 -0500

    Fix bugs and added images to menus

--- egads-game-jam-2018/Assets/Scripts/PlayerManager.cs
+++ egads-game-jam-2018/Assets/Scripts/PlayerManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerManager : MonoBehaviour
 {
     public int lives = 3;
@@ -14,16 +14,19 @@ public class PlayerManager : MonoBehaviour
 
 	void Update()
 	{
+		print("Lives: " + lives);
 	}
 
     void OnCollisionEnter(Collision col)
     {
         lives--;
+		print(col.gameObject.name);
         Destroy(col.gameObject);
 
 		if(lives <= 0)
 		{
-			Destroy(gameObject);
+            //Destroy(gameObject);
+            SceneManager.LoadScene("EndingScene");
 		}
     }
 }
