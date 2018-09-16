61fc5762d31707c229da6da0eeeb8526dd370957
Tony Nguyen <blue28275@yahoo.com>
2018-09-16 10:26:48 -0500

    Adjustments
    -particle emits
    -moved shield closer

--- egads-game-jam-2018/Assets/Scripts/HUDScript.cs
+++ egads-game-jam-2018/Assets/Scripts/HUDScript.cs
@@ -8,6 +8,8 @@ public class HUDScript : MonoBehaviour
 	PlayerManager pManagerScript;
     public Sprite deadheart;
 	public GameObject[] lives;
+	GameObject temp;
+	Image sr;
 
 	void Start()
 	{
@@ -16,25 +18,25 @@ public class HUDScript : MonoBehaviour
 
 	void Update()
 	{
-        Debug.Log(pManagerScript.lives);
-        if(pManagerScript.lives==2)
-        {
-            GameObject temp = GameObject.Find("Life Point 1");
-            Image sr = temp.GetComponent<Image>();
-            sr.sprite = deadheart;
-        }
-        else if (pManagerScript.lives == 1)
-        {
-            GameObject temp = GameObject.Find("Life Point 2");
-            Image sr = temp.GetComponent<Image>();
-            sr.sprite = deadheart;
-        }
-        else if (pManagerScript.lives == 0)
-        {
-            GameObject temp = GameObject.Find("Life Point 3");
-            Image sr = temp.GetComponent<Image>();
-            sr.sprite = deadheart;
-        }
-
-    }
+		GameObject temp;
+		switch(pManagerScript.lives)
+		{
+			case 2:
+				temp = GameObject.Find("Life Point 1");
+				sr = temp.GetComponent<Image>();
+				sr.sprite = deadheart;
+				break;
+			case 1:
+				temp = GameObject.Find("Life Point 2");
+				sr = temp.GetComponent<Image>();
+				sr.sprite = deadheart;
+				break;
+			case 0:
+				temp = GameObject.Find("Life Point 3");
+				sr = temp.GetComponent<Image>();
+				sr.sprite = deadheart;
+				break;
+			default: break;
+		}
+	}
 }
